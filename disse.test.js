330b5a8e8e022dedb84a47397fc71cbbb4f81278
ulyssespotiguara <41129570+ulyssespotiguara@users.noreply.github.com>
2021-07-19 01:31:28 -0300

    Delete disse.test.js

--- disse.test.js
+++
@@ -1,5 +0,0 @@
-const disse = require('./disse.js');
-
-test('Testing example1 file. Should output two lines: [cat sees me] and [mary likes trees]', () => {
-  expect("disse example 1").toBe('[cat sees me] and [mary likes trees]');
-});
